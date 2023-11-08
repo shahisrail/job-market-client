e85868cb98e7bda19a2bf144b483ca8f2e1a1116
shah israil <shahisrail134@gmail.com>
2023-11-08 19:00:20 +0600

    added informatoin in fotter

--- src/Pages/Fotter/Fotter.jsx
+++ src/Pages/Fotter/Fotter.jsx
@@ -1,39 +1,40 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Fotter = () => {
   return (
     <footer className="px-4 divide-y dark:bg-gray-800 dark:text-gray-100">
       <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
         <div className="lg:w-1/3">
-          <a
+          <Link to='/'
             rel="noopener noreferrer"
-            href="#"
+            
             className="flex justify-center space-x-3 lg:justify-start"
           >
-            <div >
+            <div>
               <img
                 src="https://i.ibb.co/LnyF4B3/Untitled-design-1.png"
                 alt=""
               />
             </div>
             <span className="self-center text-2xl font-semibold">
-            Job Market
+              Job Market
             </span>
-          </a>
+          </Link>
         </div>
         <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
           <div className="space-y-3">
-            <h3 className="tracki uppercase dark:text-gray-50">Product</h3>
+            <h3 className="tracki uppercase dark:text-gray-50">job</h3>
             <ul className="space-y-1">
               <li>
-                <a rel="noopener noreferrer" href="#">
-                  Features
-                </a>
+                <Link to='/addjob'>
+                  <p rel="noopener noreferrer">ADD job</p>
+                </Link>
               </li>
               <li>
                 <a rel="noopener noreferrer" href="#">
-                  Integrations
+                 About us
                 </a>
               </li>
               <li>
@@ -49,16 +50,21 @@ const Fotter = () => {
             </ul>
           </div>
           <div className="space-y-3">
-            <h3 className="tracki uppercase dark:text-gray-50">Company</h3>
+            <h3 className="tracki uppercase dark:text-gray-50">Category</h3>
             <ul className="space-y-1">
               <li>
                 <a rel="noopener noreferrer" href="#">
-                  Privacy
+                 Web devolopment
                 </a>
               </li>
               <li>
                 <a rel="noopener noreferrer" href="#">
-                  Terms of Service
+                  Digital marketing
+                </a>
+              </li>
+              <li>
+                <a rel="noopener noreferrer" href="#">
+                  Graphic deziene
                 </a>
               </li>
             </ul>
@@ -135,7 +141,7 @@ const Fotter = () => {
         </div>
       </div>
       <div className="py-6 text-sm text-center dark:text-gray-400">
-        © 1968 Company Co. All rights reserved.
+        ©2023 Company Co. All rights reserved.
       </div>
     </footer>
   );
